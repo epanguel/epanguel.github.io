71c9868065be735a95d363312f55e32bda25249d
Epanguel <34017994+Epanguel@users.noreply.github.com>
2025-06-22 15:10:04 +0900

    renew_10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "212600962"
+    serviceWorkerVersion: "1267055466"
   }
 });
